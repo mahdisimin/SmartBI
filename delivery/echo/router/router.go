c71cad87105c84d68c8cc07daa898f914e2580b0
mahdi simin <mohammdmahdisimin@gmail.com>
2026-06-21 23:33:54 +0330

    bug fix - solve cors issue

--- delivery/echo/router/router.go
+++ delivery/echo/router/router.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 )
 
 func Router() error {
@@ -13,13 +14,18 @@ func Router() error {
 	e.GET("/healthcheck", func(c *echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"status": "ok"})
 	})
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+	}))
 
 	userGroup := e.Group("/user")
 	userGroup.POST("/register", echowebframework.UserRegisterHandler)
 	userGroup.POST("/login", echowebframework.UserLoginHandler)
 	userGroup.GET("/user_profile/:id", echowebframework.UserProfileHandler)
 
-	if err := e.Start(":8080"); err != nil {
+	if err := e.Start(":8091"); err != nil {
 		return err
 	}
 	return nil
